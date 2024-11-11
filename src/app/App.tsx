1d851bf8795c3d57995a3ff1c8db2fe2f0dc8b0f
Dmitriy Eliseev <dima.eliseev.200202@gmail.com>
2024-11-11 12:51:22 +0500

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,19 @@
-import { ContactUsAuth } from "pages/contact-us-auth";
-import { ContactUsPage } from "pages/contact-us-page";
-import { MainPage } from "pages/main";
-import { Route, Routes } from "react-router-dom";
+import { ContactUsAuth } from 'pages/contact-us-auth'
+import { ContactUsPage } from 'pages/contact-us-page'
+import { MainPage } from 'pages/main'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/contact-us-auth" element={<ContactUsAuth />} />
-        <Route path="/contact-us" element={<ContactUsPage />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='/*' element={<MainPage />} />
+				<Route path='/contact-us-auth' element={<ContactUsAuth />} />
+				<Route path='/contact-us' element={<ContactUsPage />} />
+			</Routes>
+		</>
+	)
 }
 
-export default App;
+export default App
